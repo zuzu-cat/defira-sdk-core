0d75fd8ca1942e779b18c8694e50ab146df910c2
zuzu_cat <zuyichen@gmail.com>
2022-06-05 19:14:19 -0500

    yarn build

---
+++ src/entities/wone.d.ts
@@ -0,0 +1,7 @@
+import { Token } from './token';
+/**
+ * Known WONE implementation addresses, used in our implementation of Ether#wrapped
+ */
+export declare const WONE: {
+    [chainId: number]: Token;
+};
---
+++ src/entities/wone.js
@@ -0,0 +1,9 @@
+import { Token } from './token';
+/**
+ * Known WONE implementation addresses, used in our implementation of Ether#wrapped
+ */
+export const WONE = {
+    [1666600000]: new Token(1666600000, '0xcF664087a5bB0237a0BAd6742852ec6c8d69A27a', 18, 'WONE', 'Wrapped ONE'),
+    [1666700000]: new Token(1666700000, '0x7466d7d0c21fa05f32f5a0fa27e12bdc06348ce2', 18, 'WONE', 'Wrapped ONE'),
+};
+//# sourceMappingURL=wone.js.map
